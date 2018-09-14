81546da3823b7ab6a412b10530e269c4550363a5
Ulrich Eck <ulrich.eck@tum.de>
2018-09-14 14:10:37 +0200

    bumped version number, added pydev dependency

--- conanfile.py
+++ conanfile.py
@@ -4,11 +4,15 @@ import os
 
 class PyBind11Conan(ConanFile):
     name = "pybind11"
-    version = "2.2.1"
+    version = "2.2.4"
     license = "BSD Style: https://github.com/pybind/pybind11/blob/master/LICENSE"
     url = "https://github.com/ulricheck/conan-pybind11"
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
+
+    requires = (
+        "python_dev_config/[>=0.5]@camposs/stable",
+        )
 
     def source(self):
         tools.download("https://github.com/pybind/pybind11/archive/v%s.tar.gz" % self.version,
