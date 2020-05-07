be538e59c77af651f5ba07e7e0e84d601c20dd1d
Ulrich Eck <ulrich.eck@tum.de>
2020-05-07 13:30:44 +0200

    bumped version number and added option to use conan-built python + libs

--- conanfile.py
+++ conanfile.py
@@ -4,15 +4,29 @@ import os
 
 class PyBind11Conan(ConanFile):
     name = "pybind11"
-    version = "2.4.3"
+    version = "2.5.0"
     license = "BSD Style: https://github.com/pybind/pybind11/blob/master/LICENSE"
     url = "https://github.com/ulricheck/conan-pybind11"
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
 
-    requires = (
-        "python_dev_config/[>=0.5]@camposs/stable",
-        )
+    options = {
+        "with_system_python": [True, False],
+    }
+
+    default_options = {
+        "with_system_python": True
+    }
+
+    def requirements(self):
+        if self.options.with_system_python:
+            self.requires("python_dev_config/[>=0.5]@camposs/stable")
+        else:
+            self.requires("python/3.8.2@camposs/stable")
+            self.requires("python-setuptools/41.2.0@camposs/stable")
+            self.requires("python-pip/[>=19.2.3]@camposs/stable")
+            self.requires("cython/0.29.16@camposs/stable")
+            self.requires("python-numpy/1.18.4@camposs/stable")
 
     def source(self):
         tools.download("https://github.com/pybind/pybind11/archive/v%s.tar.gz" % self.version,
